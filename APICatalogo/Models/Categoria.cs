57c43238d55159f26f18184c4e39fe4a0e691470
Caio Caldas <caiorochacaldas99@gmail.com>
2022-04-21 14:39:57 -0300

    Implementando Data annotaions no projeto e fazendo segunda migração

--- APICatalogo/Models/Categoria.cs
+++ APICatalogo/Models/Categoria.cs
@@ -1,18 +1,23 @@
 ﻿using System.Collections.ObjectModel;
+using System.ComponentModel.DataAnnotations;
 
-namespace APICatalogo.Models
+namespace APICatalogo.Models { 
+  
+public class Categoria
 {
-    public class Categoria
+    public Categoria() //inicialização da chave estrageira um para muitos
     {
-        public Categoria() //inicialização da chave estrageira um para muitos
-        {
-            Produtos = new Collection<Produto>();
-        }
-        //
-        public int CategoriaId { get; set; }
-        public string? Nome { get; set; }
-        public string? ImageUrl { get; set; }
-
-        public ICollection<Produto>? Produtos { get; set; } //uma categoria pode ter um ou mais produtos
+        Produtos = new Collection<Produto>();
     }
+    [Key]
+    public int CategoriaId { get; set; }
+    [Required]
+    [StringLength(80)]
+    public string? Nome { get; set; }
+    [Required]
+    [StringLength(300)]
+    public string? ImageUrl { get; set; }
+
+    public ICollection<Produto>? Produtos { get; set; } //uma categoria pode ter um ou mais produtos
+}
 }
