8a197d2c5e297786b189bc529389b527802c6f18
Caio Caldas <caiorochacaldas99@gmail.com>
2022-04-21 15:37:01 -0300

    Criei o arquivo de controller e declarei o contexto

---
+++ APICatalogo/Controllers/ApiController.cs
@@ -0,0 +1,18 @@
+﻿using APICatalogo.Context;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace APICatalogo.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class ApiController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public ApiController(AppDbContext context)
+        {
+            _context = context;
+        }
+    }
+}
