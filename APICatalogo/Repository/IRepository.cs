9150f93d5a267ef8fc74a2413ecf648ced87a5df
Caio Caldas <caiorochacaldas99@gmail.com>
2022-04-21 19:33:00 -0300

    Class and Interface padrão crias e implementandas

---
+++ APICatalogo/Repository/IRepository.cs
@@ -0,0 +1,15 @@
+﻿using System.Linq.Expressions;
+
+namespace APICatalogo.Repository
+{
+    public interface IRepository<T>  //T é um tipo qualquer a gente quer uma classe
+    {
+        //Metodos que vamos ultilizar
+        IQueryable<T> Get();
+        T GetById(Expression<Func<T, bool>> predicate);
+        void Add(T entity);
+        void Update(T entity);
+        void Delete(T entity);
+
+    }
+}
---
+++ APICatalogo/Repository/Repository.cs
@@ -0,0 +1,45 @@
+﻿using APICatalogo.Context;
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+
+namespace APICatalogo.Repository
+{
+    public class Repository<T> : IRepository<T> where T : class //recebemos uma interface e implementamos ela 
+    {
+        protected AppDbContext _context;
+        //injetando o context
+        public Repository(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        //OBS - Toda vez que usar o Set<T> é uma refencia do contexto retorna uma instancia DbSet<T> para o acesso pelo contexto 
+
+        public IQueryable<T> Get()
+        {
+            return _context.Set<T>().AsNoTracking(); //lista de entidades e otimizando a resposta da api com AsNoTracking
+        }
+        public T GetById(Expression<Func<T, bool>> predicate)
+        {
+            return _context.Set<T>().SingleOrDefault(predicate);
+        }
+
+        public void Add(T entity)
+        {
+            _context.Set<T>().Add(entity);
+        }
+
+        public void Delete(T entity)
+        {
+            _context.Set<T>().Remove(entity);
+        }
+
+
+
+        public void Update(T entity)
+        {
+            _context.Entry(entity).State = EntityState.Modified;
+            _context.Set<T>().Update(entity);
+        }
+    }
+}
