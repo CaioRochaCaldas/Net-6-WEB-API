532900e5d0b675b2456e82ae92cdbf3c65f4065e
Caio Caldas <caiorochacaldas99@gmail.com>
2022-04-21 15:03:27 -0300

    antes de popular os dados dos campos da migration de população de dados

--- APICatalogo/Migrations/20220421175704_PopulaCategorias.cs
+++ APICatalogo/Migrations/20220421175704_PopulaCategorias.cs
@@ -6,14 +6,16 @@ namespace APICatalogo.Migrations
 {
     public partial class PopulaCategorias : Migration
     {
-        protected override void Up(MigrationBuilder migrationBuilder)
+        protected override void Up(MigrationBuilder migrationBuilder) //abaixo temos a população dos dados
         {
-
+            migrationBuilder.Sql("Insert into Categorias(Nome,ImageUrl) Values('Bebidas','bebidas.jpg')");
+            migrationBuilder.Sql("Insert into Categorias(Nome,ImageUrl) Values('Lanches','lanches.jpg')");
+            migrationBuilder.Sql("Insert into Categorias(Nome,ImageUrl) Values('Sobremesas','sobremesas.jpg')");
         }
 
-        protected override void Down(MigrationBuilder migrationBuilder)
+        protected override void Down(MigrationBuilder migrationBuilder) //abaixo temos um comando caso queiramos deletar os dados
         {
-
+            migrationBuilder.Sql("Delete from Categorias");
         }
     }
 }
