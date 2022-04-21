8cc3ee461a767b93d9bdd1983f80cec465041f54
Caio Caldas <caiorochacaldas99@gmail.com>
2022-04-21 19:59:02 -0300

    All repositories implemented in the repository folder

---
+++ APICatalogo/Repository/IProdutoRepository.cs
@@ -0,0 +1,10 @@
+﻿using APICatalogo.Models;
+
+namespace APICatalogo.Repository
+{
+    public interface IProdutoRepository : IRepository<Produto>
+    {
+        //metodo especifico de produto
+        IEnumerable<Produto> GetProdutosPorPreco(); //lista de todos os produtos por preço
+    }
+}
---
+++ APICatalogo/Repository/ProdutoRepository.cs
@@ -0,0 +1,20 @@
+﻿using APICatalogo.Context;
+using APICatalogo.Models;
+
+namespace APICatalogo.Repository
+{
+ // ProdutoRepository (Implementa metodos expecificos) <- IProductRepository <-  <- Repository ( implementa ) <- IRepository  
+    public class ProdutoRepository : Repository<Produto> ,IProdutoRepository
+    {
+        public ProdutoRepository(AppDbContext context) : base(context)
+        {
+            //contexto
+        }
+
+        public IEnumerable<Produto> GetProdutosPorPreco()
+        {
+            //metodo de especifico de Produtos de pegar produtos por preco
+            return Get().OrderBy(c => c.Preco).ToList();
+        }
+    }
+}
