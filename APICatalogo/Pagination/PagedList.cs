d96219bcbd2f53f2d5f0e2b8b815f61ae9818c15
Caio Caldas <caiorochacaldas99@gmail.com>
2022-04-22 18:48:33 -0300

    pagelist criada e ajustada

--- APICatalogo/Pagination/PagedList.cs
+++ APICatalogo/Pagination/PagedList.cs
@@ -2,21 +2,21 @@
 {
     public class PagedList<T> : List<T> // regra de paginação
     {
-        public PagedList(List<T> items,int count,int pageNumber,int pageSize)
+        public PagedList(List<T> items,int count,int pageNumber,int pageSize) //informações a serem tratadas na pagina
         {
             TotalCount = count;
             PageSize = pageSize;
             CurrentPage = pageNumber;
-            TotalPages = (int)Math.Ceiling(count / (double)pageSize);
+            TotalPages = (int)Math.Ceiling(count / (double)pageSize); //total de iten a paginar dividido por quanto ainda falta a paginar
             AddRange(items);
         }
 
         public static PagedList<T> ToPagedList(IQueryable<T> source,int pageNumber,int pageSize)
         {
-            var count = source.Count();
-            var items = source.Skip((pageNumber - 1) + pageSize).Take(pageSize).ToList();
+            var count = source.Count(); //calculo de quanto falta a paginar
+            var items = source.Skip((pageNumber - 1) + pageSize).Take(pageSize).ToList();//itens a retornar 
 
-            return new PagedList<T>(items, count, pageNumber, pageSize);
+            return new PagedList<T>(items, count, pageNumber, pageSize); //nova lista a paginar 
         }
 
         public int CurrentPage { get; private set; }
