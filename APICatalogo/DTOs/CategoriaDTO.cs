9dba2dfbb1111d99e5b3ecb7355f899b0a91c83a
Caio Caldas <caiorochacaldas99@gmail.com>
2022-04-22 14:12:17 -0300

    pacotes mapper add ao projeto e erro em arquivo dto fixed

--- APICatalogo/DTOs/CategoriaDTO.cs
+++ APICatalogo/DTOs/CategoriaDTO.cs
@@ -4,7 +4,7 @@
     {
         public int CategoriaId { get; set; }
         public string? Nome { get; set; }
-        public string? ImageUrl { get; set; 
-        public ICollection<ProdutoDTO> Produtos { get; set; }
+        public string? ImageUrl { get; set; }
+        public ICollection<ProdutoDTO>? Produtos { get; set; }
     }
 }
